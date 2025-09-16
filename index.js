feda649c4437dc6fe3f9215a45f8bf94f83f9074
Gulsim <saralievagulsim@gmail.com>
2025-09-16 16:23:54 +0300

    task-manager f2

--- index.js
+++ index.js
@@ -1,27 +1,81 @@
-let task = '';
+let tasks = [];
 let completedTaskCount = 0;
+let completedTasks = [];
 
 function showTask() {
-	console.log(task ? `Задача на данный момент: ${task}` : 'Задача отсутствует');
+	if (tasks.length === 0) {
+		console.log(`Задачи не было добавлены или они отсутствуют`);
+	}
+	if (tasks.length > 0) {
+		tasks.forEach((task, index) => {
+			console.log(`Задача №${index + 1}: ${task.title}
+    Описание: ${task.description}
+    Статус: ${task.isCompleted ? 'Выполнена' : 'Не выполнена'}
+    `);
+		});
+	}
 }
 
-function setTask(taskDescription) {
-	if (task === '') {
-		task = taskDescription;
-		console.log(`Новая задача: ${task}`);
+function setTask(taskTitle, taskDescription) {
+	if (taskTitle && taskDescription) {
+		const newTask = {
+			title: taskTitle,
+			description: taskDescription,
+			isCompleted: false,
+			createdDate: new Date(),
+			completedDate: null,
+		};
+		tasks.push(newTask);
+		console.log(`Задача ${taskTitle} добавлена в список.`);
+	}
+}
+
+function completeTask(index) {
+	if (index >= 0 && index < tasks.length) {
+		if (!tasks[index].isCompleted) {
+			tasks[index].isCompleted = true;
+			tasks[index].completedDate = new Date();
+			completedTasks.push(tasks[index]);
+			completedTaskCount++;
+			console.log(`Задача "${tasks[index].title}" выполнена`);
+		} else {
+			console.log('Задача уже выполнена');
+		}
 	} else {
-		console.log('Не могу добавить задачу, завершите или удалите предыдущую');
+		console.log(
+			'Задачи под таким индексом нет или некорректен передаваемый индекс'
+		);
 	}
 }
 
-function completeTask() {
-	if (task) {
-		console.log(`Задача выполнена: ${task}`);
-		task = '';
-		completedTaskCount += 1;
+function deleteTask(index) {
+	if (index >= 0 && index < tasks.length) {
+		if (!tasks[index].isCompleted) {
+			const response = confirm(`Таска еще не выполнена, удалить?`);
+
+			if (!response) {
+				return 'Удаление приостановлено';
+			}
+		}
+
+		tasks.splice(index, 1);
+		console.log(`Задача под индексом: ${index} была удалена.`);
+	} else {
+		console.log(
+			'Задачи под таким индексом нет или некорректен передаваемый индекс'
+		);
 	}
 }
 
-function deleteTask(taskDescription = '') {
-	task = '';
+function clearTasks() {
+	tasks = [];
+	return `Все задачи были очищены`;
 }
+
+setTask('Пройти раздел про this', 'this, методы объекта, конструктор объектов');
+setTask('Сделать домашку', 'По разделу F2');
+showTask();
+completeTask(0);
+showTask();
+// deleteTask(1);
+// clearTasks();
